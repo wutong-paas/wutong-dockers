b0461b07b9cadccb22017452122f24fa933abffa
dingpeng24001 <dingpeng24001@talkweb.com.cn>
2022-03-09 21:29:01 +0800

    add alpine,nfs-subdir,openresty

---
+++ openresty/1.19.3.2-alpine/build.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+# References:
+# 1. https://docs.docker.com/buildx/working-with-buildx/
+# 2. https://docs.docker.com/engine/reference/commandline/buildx/
+
+# Requirements:
+# 1. docker run --privileged --rm tonistiigi/binfmt --install all
+# 2. docker login -u wutongpaas -p xxx
+
+export REPO_NAME=openresty
+export VERSION=1.19.3.2-alpine
+docker buildx create --use --name mybuilder
+docker buildx build --platform linux/amd64,linux/arm64 --push -t wutongpaas/${REPO_NAME}:${VERSION} -f Dockerfile . 
+docker buildx build --platform linux/amd64,linux/arm64 --push -t swr.cn-southwest-2.myhuaweicloud.com/wutong/${REPO_NAME}:${VERSION} -f Dockerfile . 
+docker buildx rm mybuilder
