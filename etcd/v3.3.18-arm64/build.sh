0ae91d49f09dd1b9eab3a79dad716c014bee17a8
dp <me@srcio.cn>
2022-11-30 14:09:14 +0800

    add etcd docker

---
+++ etcd/v3.3.18-arm64/build.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+# 使用 arm64 机器执行
+
+export NAMESPACE=etcd
+export VERSION=v3.3.18-arm64
+# docker build -t swr.cn-southwest-2.myhuaweicloud.com/wutong/${NAMESPACE}:${VERSION} -f Dockerfile . 
+# docker build -t wutongpaas/${NAMESPACE}:${VERSION} -f Dockerfile .
+
+docker push swr.cn-southwest-2.myhuaweicloud.com/wutong/${NAMESPACE}:${VERSION}
+docker push wutongpaas/${NAMESPACE}:${VERSION}
