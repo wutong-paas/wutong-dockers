114e87a40013e1038bddc0fdda8f3d963a6171d4
poneding <poneding@gmail.com>
2023-07-17 11:10:40 +0800

    add arthas-agent and pyroscope java-agent

--- arthas/server/3.6.9/build.sh
+++ arthas/server/3.6.9/build.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-export NAMESPACE=arthas
+export NAMESPACE=arthas-server
 export VERSION=3.6.9
 docker buildx use mybuilder || docker buildx create --use --name mybuilder
 docker buildx build --platform linux/amd64,linux/arm64 --push -t swr.cn-southwest-2.myhuaweicloud.com/wutong/${NAMESPACE}:${VERSION} -f Dockerfile . 
