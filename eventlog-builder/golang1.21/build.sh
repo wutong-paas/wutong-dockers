a7cd467f845e9c2bbcbe037d9e6897636c14b06e
poneding <poneding@gmail.com>
2023-09-08 14:11:52 +0800

    add eventlog-builder golang1.21

---
+++ eventlog-builder/golang1.21/build.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+export NAMESPACE=eventlog-builder
+export VERSION=golang1.21
+docker buildx use swrbuilder || docker buildx create --use --name swrbuilder --driver docker-container --driver-opt image=swr.cn-southwest-2.myhuaweicloud.com/wutong/buildkit:stable
+docker buildx build --platform linux/amd64,linux/arm64 --push -t swr.cn-southwest-2.myhuaweicloud.com/wutong/${NAMESPACE}:${VERSION} -f Dockerfile . 
+# docker buildx rm swrbuilder
