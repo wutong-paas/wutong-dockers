e63fcbca33f5d03585b0f73278cd3ae0b0fc4327
poneding <poneding@gmail.com>
2025-08-08 14:13:09 +0800

    add docker

---
+++ docker/dind-make/build.sh
@@ -0,0 +1,7 @@
+#! /bin/bash
+
+export NAMESPACE=swr.cn-southwest-2.myhuaweicloud.com/wutong/docker
+export VERSION=dind-make
+
+docker buildx use swrbuilder || docker buildx use swrbuilder || docker buildx create --use --name swrbuilder --driver docker-container --driver-opt image=swr.cn-southwest-2.myhuaweicloud.com/wutong/buildkit:stable
+docker buildx build --platform linux/amd64,linux/arm64 --push -t swr.cn-southwest-2.myhuaweicloud.com/wutong/${NAMESPACE}:${VERSION} -f Dockerfile . 
