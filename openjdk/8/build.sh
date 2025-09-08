0fadd93fa9201287ef03e2227ad64321effe5326
poneding <poneding@gmail.com>
2025-09-08 18:04:33 +0800

    fix: update version to 8 for OpenJDK build script

--- openjdk/8/build.sh
+++ openjdk/8/build.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 export NAMESPACE=openjdk
-export VERSION=8-jdk
+export VERSION=8
 docker buildx use swrbuilder || docker buildx use swrbuilder || docker buildx create --use --name swrbuilder --driver docker-container --driver-opt image=swr.cn-southwest-2.myhuaweicloud.com/wutong/buildkit:stable
 
 docker buildx build --platform linux/amd64,linux/arm64 --push -t swr.cn-southwest-2.myhuaweicloud.com/wutong/${NAMESPACE}:${VERSION} -f Dockerfile . 
